5450f00f0113bd13747da7e8e242e5d0417f4486
Hanbyeol Shin /  David Shin / 신한별 <76985229+shinhanbyeol@users.noreply.github.com>
2024-08-07 19:05:02 +0900

    chore: problem 1  Mockup 스케줄 삭제

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,58 +1,6 @@
 import Calaendar from '@/components/Calaendar';
 
 const CalendarPage = () => {
-  // mockup data
-  const schedules = [
-    {
-      id: '1schedule',
-      title: 'test',
-      startDate: new Date(2024, 7, 1),
-      endDate: new Date(2024, 7, 4),
-      description: 'test',
-      color: '#5fa1ed',
-    },
-    {
-      id: '2schedule',
-      title: 'test2',
-      startDate: new Date(2024, 7, 2),
-      endDate: new Date(2024, 7, 7),
-      description: 'test2',
-      color: '#2bab75',
-    },
-    {
-      id: '5schedule',
-      title: 'test6',
-      startDate: new Date(2024, 7, 6),
-      endDate: new Date(2024, 7, 9),
-      description: 'test2',
-      color: '#e1ae1a',
-    },
-    // {
-    //   id: '7schedule',
-    //   title: 'test3ddd',
-    //   startDate: new Date(2024, 7, 4),
-    //   endDate: new Date(2024, 7, 5),
-    //   description: 'test555',
-    //   color: '#e1ae1a',
-    // },
-    {
-      id: '3schedule',
-      title: 'test3',
-      startDate: new Date(2024, 7, 5),
-      endDate: new Date(2024, 7, 8),
-      description: 'test2',
-      color: '#e1ae1a',
-    },
-    {
-      id: '4schedule',
-      title: 'test4',
-      startDate: new Date(2024, 7, 10),
-      endDate: new Date(2024, 7, 11),
-      description: 'test2',
-      color: '#e1ae1a',
-    },
-  ];
-
   return (
     <section className="w-full h-full">
       <Calaendar
@@ -60,7 +8,7 @@ const CalendarPage = () => {
           width: '100%',
           height: '100%',
         }}
-        schedules={schedules}
+        schedules={[]}
       />
     </section>
   );
